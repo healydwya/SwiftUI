d48589a8bb1688493ab5c022fd3f093182c0f9ea
Healy Dwyer <healydwyer@gmail.com>
2021-10-12 19:45:58 -0400

    initial text to speech

--- ACT Voice Demo WatchKit Extension/ContentView.swift
+++ ACT Voice Demo WatchKit Extension/ContentView.swift
@@ -6,11 +6,25 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+            Text("Start a Trip")
+                .font(.title)
+                .fontWeight(.thin)
+                .padding()
+                .onTapGesture {
+                let text = "Hey Mom and Dad! I am making an apple watch talk! This is cool."
+                
+                let utterance = AVSpeechUtterance(string: text)
+                let synthesizer = AVSpeechSynthesizer()
+                synthesizer.speak(utterance)
+                }
+        }.onAppear {
+
+        }
     }
 }
 
