777a664254042b79096f323b3d029df7244a2eee
Healy Dwyer <healydwyer@gmail.com>
2021-10-12 18:01:19 -0400

    Initial Commit

---
+++ ACT Voice Demo WatchKit Extension/ACT_Voice_DemoApp.swift
@@ -0,0 +1,19 @@
+//
+//  ACT_Voice_DemoApp.swift
+//  ACT Voice Demo WatchKit Extension
+//
+//  Created by Healy Dwyer on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ACT_Voice_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ ACT Voice Demo WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ACT Voice Demo WatchKit Extension
+//
+//  Created by Healy Dwyer on 10/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
